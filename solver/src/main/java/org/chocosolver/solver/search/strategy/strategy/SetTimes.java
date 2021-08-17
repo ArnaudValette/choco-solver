e37ac269535e94e5619d98519a1885a609977184
Arthur Godet <arth.godet@gmail.com>
2021-08-17 15:04:01 +0200

    fix SetTimes strategy behaviour

--- solver/src/main/java/org/chocosolver/solver/search/strategy/strategy/SetTimes.java
+++ solver/src/main/java/org/chocosolver/solver/search/strategy/strategy/SetTimes.java
@@ -7,96 +7,87 @@
  *
  * See LICENSE file in the project root for full license information.
  */
-/*
-@author Arthur Godet <arth.godet@gmail.com>
-@since 01/06/2020
-*/
 
 package org.chocosolver.solver.search.strategy.strategy;
 
+import java.util.Arrays;
+import org.chocosolver.memory.IStateInt;
+import org.chocosolver.solver.Model;
 import org.chocosolver.solver.constraints.Propagator;
 import org.chocosolver.solver.constraints.PropagatorPriority;
 import org.chocosolver.solver.exception.ContradictionException;
-import org.chocosolver.solver.search.loop.monitors.IMonitorUpBranch;
-import org.chocosolver.solver.search.strategy.decision.IntDecision;
 import org.chocosolver.solver.search.strategy.selectors.variables.VariableSelector;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.Task;
-import org.chocosolver.solver.variables.events.IntEventType;
 import org.chocosolver.util.ESat;
 
-import java.util.Arrays;
-import java.util.HashMap;
-
-import static org.chocosolver.solver.variables.events.IntEventType.*;
-
-public class SetTimes extends Propagator<IntVar> implements VariableSelector<IntVar>, IMonitorUpBranch {
+/**
+ * Search for scheduling problems described in the following paper :
+ * Godard, D., Laborie, P., Nuijten, W.: Randomized large neighborhood search for cumulative scheduling. In: Biundo, S., Myers, K.L., Rajan, K. (eds.) Proceedings of the Fifteenth International Conference on Automated Planning and Scheduling(ICAPS 2005), June 5-10 2005, Monterey, California, USA. pp. 81–89. AAAI (2005),http://www.aaai.org/Library/ICAPS/2005/icaps05-009.php
+ *
+ * Beware that the search is not complete in general, as stated in the paper.
+ *
+ * @author Arthur Godet <arth.godet@gmail.com>
+ * @since 26/05/2020
+ */
+public class SetTimes extends Propagator<IntVar> implements VariableSelector<IntVar> {
     private final Task[] tasks;
-    private final HashMap<IntVar, Integer> notSelectable;
+    private final IntVar[] starts;
+    private final int notMarked;
+    private final IStateInt[] lastStartTime;
 
     public SetTimes(Task[] tasks) {
-        super(Arrays.stream(tasks).map(Task::getStart).toArray(IntVar[]::new), PropagatorPriority.UNARY, true);
+        super(Arrays.stream(tasks).map(Task::getStart).toArray(IntVar[]::new), PropagatorPriority.VERY_SLOW, false);
         this.tasks = tasks;
-        this.notSelectable = new HashMap<>(tasks.length);
+        notMarked = Arrays.stream(tasks).mapToInt(t -> t.getStart().getLB()).min().getAsInt() - 1;
+        lastStartTime = new IStateInt[tasks.length];
+        Model model = tasks[0].getStart().getModel();
+        for(int i = 0; i < tasks.length; i++) {
+            lastStartTime[i] = model.getEnvironment().makeInt(notMarked);
+        }
+        starts = Arrays.stream(tasks).map(Task::getStart).toArray(IntVar[]::new);
     }
 
-    private IntVar selectNextVariable() {
-        int small_idx = -1;
-        int small_est = Integer.MAX_VALUE;
-        int small_lst = Integer.MAX_VALUE;
-        for (int idx = 0; idx < tasks.length; idx++) {
-            int dsize = tasks[idx].getStart().getDomainSize();
-            int est = tasks[idx].getStart().getLB();
-            int lst = tasks[idx].getEnd().getLB();
-            if (dsize > 1 && selectable(idx, est)) {
-                if(est < small_est || (est == small_est && lst < small_lst)) {
-                    small_est = est;
-                    small_lst = lst;
-                    small_idx = idx;
+    private IntVar selectNextVariable(boolean mark) {
+        int idx = -1;
+        for(int i = 0; i < tasks.length; i++) {
+            IntVar start = tasks[i].getStart();
+            if(!start.isInstantiated()) {
+                if(lastStartTime[i].get() != notMarked && lastStartTime[i].get() < start.getLB()) {
+                    lastStartTime[i].set(notMarked);
+                }
+                if(lastStartTime[i].get() == notMarked) {
+                    if(
+                        idx == -1
+                            || start.getLB() < tasks[idx].getStart().getLB()
+                            || start.getLB() == tasks[idx].getStart().getLB() && tasks[i].getEnd().getLB() < tasks[idx].getEnd().getLB()
+                    ) {
+                        idx = i;
+                    }
                 }
             }
         }
-        return small_idx > -1 ? tasks[small_idx].getStart() : null;
-    }
-
-    private boolean selectable(int idx, int est) {
-        Integer est_ = notSelectable.get(tasks[idx].getStart());
-        return est_ == null || est != est_;
-    }
-
-    @Override
-    public void beforeUpBranch() {
-        if(this.getModel().getSolver().getDecisionPath().size()>1) {
-            IntDecision dec = (IntDecision) this.getModel().getSolver().getDecisionPath().getLastDecision();
-            if (dec.hasNext()) {
-                notSelectable.put(dec.getDecisionVariable(), dec.getDecisionVariable().getLB());
+        if(idx != -1) {
+            if(mark) {
+                lastStartTime[idx].set(starts[idx].getLB() + 1); // + 1 because start is updated on refuted branch
             }
+            return tasks[idx].getStart();
+        } else {
+            return null;
         }
     }
 
     @Override
     public IntVar getVariable(IntVar[] variables) {
-        return selectNextVariable();
+        return selectNextVariable(true);
     }
 
     @Override
     public void propagate(int evtmask) throws ContradictionException {
-        //if (!isCompletelyInstantiated() && selectNextVariable() == null) {
-        //    fails();
-        //}
-    }
-
-    @Override
-    public int getPropagationConditions(int vIdx) {
-        return IntEventType.combine(INCLOW, INSTANTIATE);
-    }
-
-    @Override
-    public void propagate(int idxVarInProp, int mask) throws ContradictionException {
-        IntVar start =  tasks[idxVarInProp].getStart();
-        Integer est = notSelectable.get(start);
-        if(est!=null && est > start.getLB()){
-            notSelectable.remove(start);
+        if(isCompletelyInstantiated()) {
+            setPassive();
+        } else if(selectNextVariable(false) == null) {
+            fails();
         }
     }
 
