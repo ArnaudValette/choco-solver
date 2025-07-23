25acb23a3d932704e21384f625c8b3bbbef985aa
arnaudvalette <contact@valettearnaud.dev>
2025-07-23 18:01:48 +0200

    [impl] SACNaive

--- solver/src/main/java/org/chocosolver/solver/propagation/consistencyStrategy/SACNaiveDriverStrategy.java
+++ solver/src/main/java/org/chocosolver/solver/propagation/consistencyStrategy/SACNaiveDriverStrategy.java
@@ -1,52 +1,83 @@
 package org.chocosolver.solver.propagation.consistencyStrategy;
 
-import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.constraints.graph.symmbreaking.Pair;
 import org.chocosolver.solver.exception.ContradictionException;
 import org.chocosolver.solver.propagation.SingletonConsistencyEngine;
 import org.chocosolver.solver.propagation.consistencyStrategy.types.PairBasedStrategy;
+import org.chocosolver.solver.propagation.consistencyStrategy.types.PassConsumer;
 import org.chocosolver.solver.search.strategy.decision.IntDecision;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.util.objects.queues.ReinitialisableQueue;
 
-public class SACNaiveDriverStrategy extends PairBasedStrategy {
-
+public class SACNaiveDriverStrategy extends PairBasedStrategy implements PassConsumer {
+    public boolean consumePasses = false;
     public SACNaiveDriverStrategy(){
         super();
+    }
+
+    public SACNaiveDriverStrategy consumePasses(){
+        consumePasses=true;
+        return this;
+    }
+
+    public SACNaiveDriverStrategy ignorePasses(){
+        consumePasses=false;
+        return this;
     }
 
     public SACNaiveDriverStrategy(ReinitialisableQueue<Pair<IntVar,Integer>> Q) {
         super(Q);
     }
 
-    private void acEnforce(SingletonConsistencyEngine E) throws ContradictionException {
+    public void centralRoutine(SingletonConsistencyEngine E) throws ContradictionException {
         E.doPropagate();
+    }
+
+    @Override
+    public boolean willConsumePasses() {
+        return consumePasses;
+    }
+
+    private void initState(SingletonConsistencyEngine E){
+        E.setDoConsumePasses(false);
+        E.setCheckSingleton(false);
+        E.setDoFilterScheduling(false);
+        E.setBlockLateScheduling(true);
     }
 
     @Override
     public void propagate(SingletonConsistencyEngine E) throws ContradictionException {
         super.propagate(E);
-        E.setDoConsumePasses(false);
-        E.setCheckSingleton(false);
-        E.setDoFilterScheduling(false);
-        E.setBlockLateScheduling(true);
+        initState(E);
+
         Q.reinit();
-        acEnforce(E);
+        centralRoutine(E);
+
         while (!Q.isEmpty()) {
+
+
             Pair<IntVar, Integer> p = Q.pop();
             IntVar v = p.getA();
             Integer val = p.getB();
+
             E.worldPush();
             IntDecision d = E.decide(v, val);
             d.buildNext();
+
             try {
                 d.apply();
-                acEnforce(E);
-                E.worldPopNFlush();
+                onBeforePasses(E);
+                centralRoutine(E);
+                doConsumePasses(E);
+
+                E.worldPop();
             } catch (ContradictionException e) {
                 E.worldPopNFlush();
                 v.removeValue(val, E);
-                acEnforce(E);
+
+                onAfterPasses(E);
+
+                centralRoutine(E);
             }
         }
     }
